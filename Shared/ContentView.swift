02be3da9493991ff6d10f66d09f49460d1ab6008
Dominik Hauser <dom@dasdom.de>
2021-01-14 16:37:32 +0100

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,18 @@
+//  Created by dasdom on 14.01.21.
+//  
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/TestSampleApp.swift
@@ -0,0 +1,14 @@
+//  Created by dasdom on 14.01.21.
+//  
+//
+
+import SwiftUI
+
+@main
+struct TestSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
